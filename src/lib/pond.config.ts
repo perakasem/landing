0584d5fb548830a2a271969cb6cb9849a5c4fbb9
perakasem <pkasemsripitak@gmail.com>
2025-03-04 17:32:38 -0500

    Pond Updates

--- src/lib/pond.config.ts
+++ src/lib/pond.config.ts
@@ -10,12 +10,12 @@ export const watch = 'On Information Hazards'
 export const watchSource = 'Anders Sandberg'
 
 export const mediaUrl = "https://youtu.be/F0cdbR5ognY?si=L6HZyjQ2UGlSyJI6"
-export const media = 'Denial is a River (in Egypt, your husband is gay)'
+export const media = 'Your Husband is Gay (video)'
 export const mediaSource = 'Doechii'
 
-export const readUrl = "https://forum.effectivealtruism.org/posts/3ryvsSZWd5vqgY7bg/how-ai-takeover-might-happen-in-two-years?"
-export const read = "A Looming AI Takeover"
-export const readSource = "Joshua Clymer"
+export const readUrl = "https://www.science.org/doi/10.1126/sciadv.adr4797"
+export const read = "Remote Tasting"
+export const readSource = "Chen et al."
 
 export const artworkSrc = "/artwork.png"
 export const artwork = "Foundations of Flavor"
